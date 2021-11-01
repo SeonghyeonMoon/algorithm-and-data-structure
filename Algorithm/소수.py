67af7d2d5a3d440f8a1c445ee9ac713670f31a0b
Sherty <75469212+shine7329@users.noreply.github.com>
2021-11-01 21:58:22 +0900

    Add files via upload

--- Algorithm/소수.py
+++ Algorithm/소수.py
@@ -4,6 +4,9 @@ def get_prime_number(n):
     ck = [False] * 2 + [True for i in range(n - 1)]
     for i in range(2, int((n + 1) ** 0.5) + 1):
         if ck[i] == True:
-            for j in range(i + i, n + 1, i):
+            for j in range(i * i, n + 1, i):
                 ck[j] = False
     return ck
+
+
+print(get_prime_number(16))
--- Web/term.md
+++ Web/term.md
@@ -1,3 +1,7 @@
 # WEB 용어정리
 
 1. SEO - Search Engine Optimization
+
+2. HTTP - HyperText Transfer Protocol - Client-Server 통신 프로토콜
+
+3. RESTAPI - POST = Create, PUT = Update
