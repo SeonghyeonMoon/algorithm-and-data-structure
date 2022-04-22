0b5e6b8c7fed55418c603089d10e6f53df47fe37
Seonghyeon Moon <shine7329@gmail.com>
2022-04-22 20:40:16 +0900

    문제풀이

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
 import sys
-from tkinter.tix import Tree
 sys.stdin = open('input.txt')
+input = sys.stdin.readline
 
-from collections import defaultdict
+def solution(n):
+    if n == 1: return 1
+    a, b = 1, 3
+    for _ in range(n - 2):
+        b, a = (a * 2 + b) % 10007, b
+    return b
 
-N, M, B = map(int, input().split())
-dic = defaultdict(int)
-
-max_height = 0
-min_height = 256 
-
-for _ in range(N):
-    for height in list(map(int, input().split())):
-        dic[height] += 1
-
-for key in dic.keys():
-    max_height = max(max_height, key)
-    min_height = min(min_height, key)
-
-answer = [500 * 500 * 256, 0]
-for height in range(min_height, max_height + 1):
-    need_block = 0
-    time = 0
-    for target_height, target_number in dic.items():
-        gap = target_height - height
-        need_block -= target_number * gap
-        if gap > 0:
-            time += 2 * target_number * gap
-        else:
-            time -= 1 * target_number * gap
-    if need_block > B:
-        break
-    if answer[0] >= time:
-        answer = [time, height]
-
-print(answer[0], answer[1])
+print(solution(int(input())))
