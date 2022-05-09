47ea50eb17754b75892f13a8a3a74480a9e78b4d
Seonghyeon Moon <shine7329@gmail.com>
2022-05-09 21:19:27 +0900

    문제 풀이

--- main.py
+++ main.py
@@ -4,28 +4,13 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 
-def solution():
-    p = input().rstrip()
-    n = int(input())
-    numbers = deque(input()[1:-2].split(','))
+graph = {}
 
-    if n == 0:
-        numbers = []
-    is_reversed = False
-    for function in p:
-        if function == 'R':
-            is_reversed = not is_reversed
-        else:
-            if not numbers:
-                return 'error'
-            if is_reversed:
-                numbers.pop()
-            else:
-                numbers.popleft()
-    if is_reversed:
-        numbers.reverse()
-    return f'[{",".join(numbers)}]'
+for i in range(4):
+    input_data = list(map(int, input().split()))
+    for j in range(4):
+        number = input_data[2 * j]
+        direction = input_data[2 * j + 1]
+        graph[number] = [i, j, direction]
 
-T = int(input())
-for _ in range(T):
-    print(solution())
+print(graph)
