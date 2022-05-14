cba8733103b2c35a5acd9c3a8a2ca0c7a4513e1b
Seonghyeon Moon <shine7329@gmail.com>
2022-05-14 17:01:49 +0900

    일일기록 정리

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from collections import deque
+from math import ceil, floor
 import sys
-sys.stdin = open('input.txt')
+from tkinter import N
+from typing import Counter
+# sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 
-graph = {}
 
-for i in range(4):
-    input_data = list(map(int, input().split()))
-    for j in range(4):
-        number = input_data[2 * j]
-        direction = input_data[2 * j + 1]
-        graph[number] = [i, j, direction]
+def solution(A, F, M):
+    expected_sum = (len(A) + F) * M
+    needed_sum = expected_sum - sum(A)
+    if needed_sum > F * 6 or needed_sum < F * 1:
+        return [0]
+    if needed_sum % F:
+        return [needed_sum % F + needed_sum // F] + [needed_sum // F for _ in range(F - 1)]
+    return [needed_sum // F for _ in range(F)]
 
-print(graph)
+
+
+print(solution([1, 5, 6], 4, 3))
