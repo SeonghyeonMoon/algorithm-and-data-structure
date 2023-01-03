2509bc06f5aa59986c7b366f6a81427bfd51563d
문성현 <shine7329@gmail.com>
2023-01-03 14:31:20 +0900

    [프로그래머스] 점 찍기

---
+++ [프로그래머스] 점 찍기.js
@@ -0,0 +1,10 @@
+const solution = (k, d) => {
+  let result = 0;
+  for (let x = 0; x <= d; x += k) {
+    const maxY = Math.sqrt(d ** 2 - x ** 2);
+    result += Math.floor(maxY / k) + 1;
+  }
+  return result;
+};
+
+console.log(solution(2, 4));
