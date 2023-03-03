7abcb1fffa6aa89ef78d3bf82cd62cd8b8d54afd
문성현 <shine7329@gmail.com>
2023-03-03 11:16:09 +0900

    heap compare 메소드 통해서 값 비교하도록 변경

--- MaxHeap.js
+++ MaxHeap.js
@@ -1,15 +1,25 @@
-class MinHeap {
+class MaxHeap {
   heap = [];
 
   get length() {
     return this.heap.length;
   }
 
+  compare(a, b) {
+    if (Array.isArray(a) && Array.isArray(b)) {
+      for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) return a[i] > b[i];
+      }
+      return false;
+    }
+    return a > b;
+  }
+
   push(value) {
     let targetIndex = this.heap.length;
     this.heap.push(value);
     let parentIndex = Math.floor((targetIndex - 1) / 2);
-    while (targetIndex > 0 && this.heap[parentIndex] > value) {
+    while (targetIndex > 0 && this.compare(value, this.heap[parentIndex])) {
       this.swap(parentIndex, targetIndex);
       targetIndex = parentIndex;
       parentIndex = Math.floor((targetIndex - 1) / 2);
@@ -28,8 +38,8 @@ class MinHeap {
     const leftIndex = index * 2 + 1;
     const rightIndex = index * 2 + 2;
     let minIndex = index;
-    if (this.heap[leftIndex] < this.heap[minIndex]) minIndex = leftIndex;
-    if (this.heap[rightIndex] < this.heap[minIndex]) minIndex = rightIndex;
+    if (this.compare(this.heap[leftIndex], this.heap[minIndex])) minIndex = leftIndex;
+    if (this.compare(this.heap[rightIndex], this.heap[minIndex])) minIndex = rightIndex;
     if (minIndex !== index) {
       this.swap(minIndex, index);
       this.bubbleDown(minIndex);
